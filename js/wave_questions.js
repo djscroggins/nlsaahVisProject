6f38d9d31125b61d7722255395d91b0974057eb9
Caroline Cao <caroline.cao39@gmail.com>
2017-11-03 00:01:38 -0500

    wave_questions_v1

---
+++ js/wave_questions.js
@@ -0,0 +1,121 @@
+{
+	var wave_questions={"wave_1": {
+	    "H1DA_DailyActivity":{
+	    	"H1DA1":"During the past week, how many times did you do work around the house, such as cleaning, cooking, laundry, yardwork, or caring for a pet?",
+			"H1DA2":"During the past week, how many times did you do hobbies, such as collecting baseball cards, playing a musical instrument, reading, or doing arts and crafts?",
+			"H1DA3":"During the past week, how many times did you watch television or videos, or play video games?",
+			"H1DA4":"During the past week, how many times did you go roller-blading, roller-skating, skate-boarding, or bicycling?",
+			"H1DA5":"During the past week, how many times did you play an active sport, such as baseball, softball, basketball, soccer, swimming, or football?",
+			"H1DA6":"During the past week, how many times did you do exercise, such as jogging, walking, karate, jumping rope, gymnastics or dancing?",
+			"H1DA7":"During the past week, how many times did you just hang out with friends?",
+			"H1DA8":"How many hours a week do you watch television?",
+			"H1DA9":"How many hours a week do you watch videos?",
+			"H1DA10":"How many hours a week do you play video or computer games?",
+			"H1DA11":"How many hours a week do you listen to the radio?"
+		},
+		"H1GH_GeneralHealth":{
+			"H1GH1":"In general, how is your health? Would you say...",
+			"H1GH1A":"Do you have difficulty using your hands, arms, legs, or feet because of a physical condition that has lasted for the past twelve months or more?",
+			"H1GH2":"How often have you had a headache?",
+			"H1GH3":"feeling hot all over suddenly, for no reason",
+			"H1GH4":"a stomach ache or an upset stomach",
+			"H1GH5":"cold sweats",
+			"H1GH6":"feeling physically weak, for no reason",
+			"H1GH7":"a sore throat or a cough",
+			"H1GH8":"feeling very tired, for no reason",
+			"H1GH9":"painful or very frequent urination (or peeing)",
+			"H1GH10":"feeling really sick",
+			"H1GH11":"waking up feeling tired",
+			"H1GH12":"skin problems, such as itching or pimples",
+			"H1GH13":"dizziness",
+			"H1GH14":"chest pains",
+			"H1GH15":"aches, pains, or soreness in your muscles or joints",
+			"H1GH17":"poor appetite",
+			"H1GH18":"trouble falling asleep or staying asleep",
+			"H1GH19":"trouble relaxing",
+			"H1GH20":"moodiness",
+			"H1GH21":"frequent crying",
+			"H1GH22":"fearfulness",
+			"H1GH24":"When did you last have a physical examination by a doctor or nurse?",
+			"H1GH25":"When did you last have a dental examination by a dentist or hygienist?",
+			"H1GH26":"Has there been any time over the past year when you thought you should get medical care, but you did not?",
+			"H1GH28":"How do you think of yourself in terms of weight?",
+			"H1GH29":"Are you trying to lose weight, gain weight, or stay the same weight?",
+			"H1GH32":"How often did you drink milk, or eat yogurt, or cheese yesterday?",
+			"H1GH33":"How often did you eat fruit or drink fruit juice yesterday?",
+			"H1GH34":"How often did you eat vegetables yesterday?",
+			"H1GH35":"How often did you eat bread, cereal, pretzels, rice, or pasta yesterday?",
+			"H1GH36":"How often did you eat cookies, doughnuts, pie, or cake yesterday?"
+		},
+		"H1HS_AccessToHealthServices":{
+			"H1HS1":"In the past year have you had a routine physical examination?",
+			"H1HS7":"In the past year, have you received family planning counseling or services?"
+		},
+		"H1TO_TobaccoAlcoholDrugs":{
+			"H1TO1":"Have you ever tried cigarette smoking, even just 1 or 2 puffs?",
+			"H1TO2":"How old were you when you smoked a whole cigarette for the first time?",
+			"H1TO3":"Have you ever smoked cigarettes regularly, that is, at least 1 cigarette every day for 30 days?",
+			"H1TO4":"How old were you when you first started smoking cigarettes regularly (at least 1 cigarette every day for 30 days)?"
+		}
+	}
+
+	"wave_2": {
+		
+	}
+
+	"wave_3": {
+		"question1":""
+		"question2":""
+		"question3":""
+		"question4":""
+		"question5":""
+		"question6":""
+		"question7":""
+		"question8":""
+		"question9":""
+		"question10":""
+		"question11":""
+		"question12":""
+		"question13":""
+		"question14":""
+		"question15":""
+		"question16":""
+		"question17":""
+		"question18":""
+		"question19":""
+		"question20":""
+		"question21":""
+		"question22":""
+		"question23":""
+		"question24":""
+		"question25":""
+	}
+
+	"wave_4": {
+		"question1":""
+		"question2":""
+		"question3":""
+		"question4":""
+		"question5":""
+		"question6":""
+		"question7":""
+		"question8":""
+		"question9":""
+		"question10":""
+		"question11":""
+		"question12":""
+		"question13":""
+		"question14":""
+		"question15":""
+		"question16":""
+		"question17":""
+		"question18":""
+		"question19":""
+		"question20":""
+		"question21":""
+		"question22":""
+		"question23":""
+		"question24":""
+		"question25":""
+	}
+}
